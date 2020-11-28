47e6ced5ed7f8a11ca0060b7e8e753a9986fd69c
Mahmoud Feraas <mahmoudferaas7@gmail.com>
2020-11-28 12:19:03 +0200

    add createUserCommandHandler tests

---
+++ Test/Terkwaz.IssueTracker.Application.UnitTest/IssueTypes/Commands/Create/CreateIssueTypeCommandHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Terkwaz.IssueTracker.Application.Features.IsseTypes.Commands.Create;
+using Terkwaz.IssueTracker.Application.UnitTest.Common;
+using Terkwaz.IssueTracker.Domain.Entities;
+using Xunit;
+
+namespace Terkwaz.IssueTracker.Application.UnitTest.IssueTypes.Commands.Create
+{
+    public class CreateIssueTypeCommandHandlerTests : CommandTestBase
+    {
+        [Theory]
+        [NoRecursion]
+        public async Task Handle_ValidCommand_ShouldSaveEntriesSuccessfully(IssueType issueType)
+        {
+            // Arrange
+            _mapperMock.Setup(m => m.Map<IssueType>(It.IsAny<CreateIssueTypeCommand>()))
+               .Returns(issueType); // AutoMapper setup
+
+            var sut = new CreateIssueTypeCommandHandler(_context, _mapperMock.Object); // creating system under test
+
+            // Act
+            await sut.Handle(new CreateIssueTypeCommand(), CancellationToken.None);
+
+            // Assert
+            _context.IssueTypes.Count().ShouldBe(1);
+        }
+    }
+}
---
+++ Test/Terkwaz.IssueTracker.Application.UnitTest/IssueTypes/Commands/Create/CreateIssueTypeCommandValidatorTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation.Validators;
+using FluentValidation.Validators.UnitTestExtension.Composer;
+using FluentValidation.Validators.UnitTestExtension.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Terkwaz.IssueTracker.Application.Features.IsseTypes.Commands.Create;
+using Terkwaz.IssueTracker.Application.UnitTest.Common;
+using Xunit;
+
+namespace Terkwaz.IssueTracker.Application.UnitTest.IssueTypes.Commands.Create
+{
+    public class CreateIssueTypeCommandValidatorTests : CommandTestBase
+    {
+        private readonly CreateIssueTypeCommandValidators _validator;
+
+        public CreateIssueTypeCommandValidatorTests()
+        {
+            _validator = new CreateIssueTypeCommandValidators();
+        }
+
+        [Fact]
+        public void GivenWhenValidatorConstructing_ThenNPropertiesShouldHaveRules()
+        {
+            // Assert
+            _validator.ShouldHaveRulesCount(1);
+        }
+
+        [Fact]
+        public void Given_Name_IsEmpty_When_Validating_Then_Error()
+        {
+            // Assert
+            _validator.ShouldHaveRules(x => x.Name, BaseVerifiersSetComposer.Build()
+                .AddPropertyValidatorVerifier<NotEmptyValidator>()
+                .Create());
+        }
+
+        
+    }
+}
