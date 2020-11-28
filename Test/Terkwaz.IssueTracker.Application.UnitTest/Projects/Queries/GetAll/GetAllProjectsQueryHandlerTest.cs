7cef5129fa77721cdbe14e477e3103b36ea16b05
Mahmoud Feraas <mahmoudferaas7@gmail.com>
2020-11-28 18:03:16 +0200

    add GetAllProjectsQueryHandler test

---
+++ Test/Terkwaz.IssueTracker.Application.UnitTest/Projects/Queries/GetAll/GetAllProjectsQueryHandlerTest.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Terkwaz.IssueTracker.Application.Common.Dtos;
+using Terkwaz.IssueTracker.Application.Features.Projects.Queries.GetAll;
+using Terkwaz.IssueTracker.Application.UnitTest.Common;
+using Terkwaz.IssueTracker.Domain.Entities;
+using Xunit;
+
+namespace Terkwaz.IssueTracker.Application.UnitTest.Projects.Queries.GetAll
+{
+    public class GetAllProjectsQueryHandlerTest : CommandTestBase
+    {
+        private readonly IFixture _fixture;
+
+        public GetAllProjectsQueryHandlerTest()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        [Theory]
+        [NoRecursion]
+        public async Task Handle_GetAllQuery_ShouldReturnEntriesSuccessfully(List<ProjectDto> output)
+        {
+            // Arrange
+            _mapperMock.Setup(m => m.Map<List<ProjectDto>>(It.IsAny<List<Project>>())).Returns(output); // AutoMapper setup
+
+            var sut = new GetAllProjectsQueryHandler(_context, _mapperMock.Object); // creating system under test
+
+            var temProject = _fixture.Create<Project>();
+
+            // Act
+            await ContextOperation.CreateEntity(_context, temProject);
+
+            var result = await sut.Handle(new GetAllProjectsQuery(), CancellationToken.None);
+
+            // Assert
+            result.Count().ShouldBeGreaterThan(0);
+        }
+    }
+}
